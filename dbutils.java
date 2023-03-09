4c5503c15a3c22c4fef401a86ce856ae2ad258ab
sreekanth45k <sreekanthk110@gmail.com>
2023-03-09 03:42:04 +0000

    updated in dev branch

--- dbutils.java
+++ dbutils.java
@@ -1,1 +1,2 @@
 understanding the conecept of the process
+A compound of development (dev) and oparations (ops).Devops is the union of people process and technology continually value to the customers
